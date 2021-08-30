3b2aa293457f52fc9aeeab5695016e0c4e075230
Nelson Estevão <nelsonmestevao@gmail.com>
2021-08-30 22:26:28 +0100

    Fix background curve (#4)

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -4,16 +4,21 @@ import Footer from '~/components/Footer';
 import styles from './style.module.css';
 
 const Layout = ({ children, title }) => (
-  <div className={styles.container}>
-    <Head>
-      <title>{title}</title>
-      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
-      <link rel="icon" href="/favicon.ico" type="image/x-icon" />
-    </Head>
+  <div style={{ position: 'relative' }}>
+    <div className={styles.rectangle} />
+    <div className={styles.curve} />
+    <div className={styles.base} />
+    <div className={styles.container}>
+      <Head>
+        <title>{title}</title>
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
 
-    <main className={styles.main}>{children}</main>
+      <main className={styles.main}>{children}</main>
 
-    <Footer isThemeDark />
+      <Footer isThemeDark />
+    </div>
   </div>
 );
 
